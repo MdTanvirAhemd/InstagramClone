3eb1e6726d643f01788e09e1480e95e9b2454586
Overlord <tanvir1005@student.sust.edu>
2021-10-13 21:48:04 +0600

    Adding new activity

--- app/src/main/java/com/ahmed/instagramclone/SignUpLoginActivity.java
+++ app/src/main/java/com/ahmed/instagramclone/SignUpLoginActivity.java
@@ -1,4 +1,20 @@
 package com.ahmed.instagramclone;
 
-public class SignUpLoginActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.signup_login_activity);
+
+
+
+    }
 }
